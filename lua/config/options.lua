9580654352428cf598b001b44dc038b9ea2a4f6e
celino <>
2024-05-18 22:29:58 +0200

    + options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,2 +1,34 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+-- base options
+vim.opt.mouse = "a"
+vim.opt.undofile = true
+vim.opt.updatetime = 200
+vim.opt.timeoutlen = 500
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.clipboard = "unnamedplus"
+
+-- ui options
+vim.opt.showmode = false
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+vim.opt.scrolloff = 10
+vim.opt.wrap = false
+
+-- search
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
+vim.opt.inccommand = "split"
+
+-- tabs & indent
+local TAB = 2
+vim.opt.expandtab = true
+vim.opt.tabstop = TAB
+vim.opt.softtabstop = TAB
+vim.opt.shiftwidth = TAB
+vim.opt.autoindent = true
+vim.opt.breakindent = true
