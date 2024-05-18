4f2048da7a97704bce427060fc668bceaa70bb1a
celino <>
2024-05-18 22:27:01 +0200

    + telescope

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 local nmap = function(key, bind, desc)
 	vim.keymap.set("n", key, bind, { desc = desc })
 end
+
+local telescope = require("telescope.builtin")
+
+-- [f]ind
+nmap("<leader>ff", telescope.find_files, "[f]ind [f]ile")
+nmap("<leader>fw", telescope.live_grep, "[f]ind [w]ord")
+nmap("<leader>fb", telescope.buffers, "[f]ind [b]uffer")
+nmap("<leader>fg", telescope.git_files, "[f]ind [g]it")
+nmap("<leader>fh", telescope.help_tags, "[f]ind [h]elp")
+nmap("<leader>fj", telescope.jumplist, "[f]ind [j]umplist")
+nmap("<leader>fk", telescope.keymaps, "[f]ind [k]eymaps")
